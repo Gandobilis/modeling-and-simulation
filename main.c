61b0365d6d817d6ff4730c65299b1f07ddf392f8
Gandobilis <lashadeveloper@gmail.com>
2023-10-22 03:25:42 +0400

    function call with exercise 5 parameters added.

--- main.c
+++ main.c
@@ -19,7 +19,8 @@ ull *exercise4_5(ull a, ull c, ull b, ull z0, ull n) {
 
 int main() {
     ull n = 1000;
-    ull *result = exercise4_5(1031, 113, 10, 1103, n);
+//    ull *result = exercise4_5(1031, 113, 10, 1103, n);
+    ull *result = exercise4_5(3074, 51, 11, 1203, n);
     for (int i = 0; i < n; i++) {
         printf("%llu\n", result[i]);
     }
