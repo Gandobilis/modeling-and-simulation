fe6bc3b0ab1ca6360c74b502b72112c9969ae6e0
Gandobilis <lashadeveloper@gmail.com>
2023-10-28 00:35:46 +0400

    Exercise 6, 8 and 9.

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 
 typedef unsigned long long ull;
 
-int exercise3(int dividend, int divisor) {
+ull exercise3(ull dividend, ull divisor) {
     while (dividend >= divisor) {
-        int dividend_bits = 0;
-        int divisor_bits = 0;
-        int temp = divisor;
+        ull dividend_bits = 0;
+        ull divisor_bits = 0;
+        ull temp = divisor;
 
         while (temp > 0) {
             temp >>= 1;
@@ -20,7 +20,7 @@ int exercise3(int dividend, int divisor) {
             dividend_bits++;
         }
 
-        int shift_count = dividend_bits - divisor_bits;
+        ull shift_count = dividend_bits - divisor_bits;
         dividend -= (divisor << shift_count);
     }
 
@@ -41,18 +41,18 @@ ull *exercise4_5_7(ull a, ull c, ull b, ull z0, ull n) {
     return result;
 }
 
-//ull *exercise6_8_9(ull a, ull b, ull q, ull z0, ull n) {
-//    ull *result = (ull *) malloc(sizeof(ull) * n);
-//
-//    ull z = z0;
-//
-//    for (int i = 0; i < n; i++) {
-//        result[i] = z;
-//        z = (a * z + c) << x >> x;
-//    }
-//
-//    return result;
-//}
+ull *exercise6_8_9(ull a, ull b, ull q, ull z0, ull n) {
+    ull *result = (ull *) malloc(sizeof(ull) * n);
+
+    ull z = z0;
+
+    for (int i = 0; i < n; i++) {
+        result[i] = z;
+        z = exercise3(a * z, (1u << b) - q);
+    }
+
+    return result;
+}
 
 int main() {
     //    // 1
@@ -69,9 +69,11 @@ int main() {
 //    ull exercise2 = (((a << 8) + 7) << x >> x) + (b >> 8);
 //    printf("%llu", exercise2);
 
-//    ull n = 1000;
-    // 3
-    printf("%d", exercise3(630360016, (1u << 31) - 1));
+//    // 3
+//    ull _exercise3 = exercise3(630360016, (1u << 31) - 1);
+//    printf("%llu", _exercise3);
+
+    ull n = 100;
     // 4
 //    ull *result = exercise4_5_7(1031, 113, 10, 1103, n);
 
@@ -81,12 +83,20 @@ int main() {
     // 7
 //    ull *result = exercise4_5_7(201, 13, 9, 2103, n);
 
-
-//    for (int i = 0; i < n; i++) {
-//        printf("%llu\n", result[i]);
-//    }
+    // 6
+    ull *result = exercise6_8_9(1604, 11, 3, 2103, n);
 //
-//    free(result);
+//    // 8
+//    ull *result = exercise6_8_9(856, 9, 3, 385, n);
+//
+//    // 9
+//    ull *result = exercise6_8_9(187, 8, 5, 119, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%llu\n", result[i]);
+    }
+
+    free(result);
 
     return 0;
 }
