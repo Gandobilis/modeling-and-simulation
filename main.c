534e29ae609e547f74173930754956b7f6c388f2
Gandobilis <lashadeveloper@gmail.com>
2023-11-17 21:21:26 +0400

    code refactored.

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 typedef unsigned long long ull;
 
-// (2^p1*a + c)%2^p2 + b / 2^p3
+// (2^p1*a + c)%2^p2 + b / 2^p3 (ზოგადი სახე)
 void exercise1_2(ull p1, ull a, ull c, ull p2, ull b, ull p3) {
     ull x = 64 - p2;
     ull result = (((a << p1) + c) << x >> x) + (b >> p3);
@@ -12,39 +12,9 @@ void exercise1_2(ull p1, ull a, ull c, ull p2, ull b, ull p3) {
 
 ull exercise3(ull dividend, ull divisor) {
     while (dividend >= divisor) {
-        ull dividend_bits = 0;
-        ull divisor_bits = 0;
-        ull temp = divisor;
-
-        while (temp > 0) {
-            temp >>= 1;
-            divisor_bits++;
-        }
-
-        temp = dividend;
-        while (temp > 0) {
-            temp >>= 1;
-            dividend_bits++;
-        }
-
-        ull shift_count = dividend_bits - divisor_bits;
-        dividend -= (divisor << shift_count);
+        dividend -= divisor;
     }
-
     return dividend;
-}
-
-ull *exercise6_8_9(ull a, ull b, ull q, ull z0, ull n) {
-    ull *result = (ull *) malloc(sizeof(ull) * n);
-
-    ull z = z0;
-
-    for (int i = 0; i < n; i++) {
-        result[i] = z;
-        z = exercise3(a * z, (1u << b) - q);
-    }
-
-    return result;
 }
 
 ull *exercise4_5_7(ull a, ull c, ull b, ull z0, ull n) {
@@ -61,6 +31,19 @@ ull *exercise4_5_7(ull a, ull c, ull b, ull z0, ull n) {
     return result;
 }
 
+ull *exercise6_8_9(ull a, ull b, ull q, ull z0, ull n) {
+    ull *result = (ull *) malloc(sizeof(ull) * n);
+
+    ull z = z0;
+
+    for (int i = 0; i < n; i++) {
+        result[i] = z;
+        z = exercise3(a * z, (1u << b) - q); // სიმულაციური გაყოფა
+    }
+
+    return result;
+}
+
 int main() {
     // 1
 //    exercise1_2(7,(int) 1542.567,3,11,(int) 209129.238754,8);
@@ -68,8 +51,13 @@ int main() {
     //2
 //    exercise1_2(8,(int) 98710293.53201,7,10,(int) 189029.238,8);
 
-    // elements number to generate
+
+    // 3
+//    printf("%llu", exercise3(630360016, (1u << 31) - 1));
+
+    // ელემენტთა რაოდენობა გენერირებულ მიმდევრობაში
     ull n = 1000;
+
     // 4
     ull *result = exercise4_5_7(1031, 113, 10, 1103, n);
 
@@ -79,24 +67,20 @@ int main() {
     //7
 //    ull *result = exercise4_5_7(201, 13, 9, 2103, n);
 
-//    // 3
-//    ull _exercise3 = exercise3(630360016, (1u << 31) - 1);
-//    printf("%llu", _exercise3);
 
-//    // 6
+    // 6
 //    ull *result = exercise6_8_9(1604, 11, 3, 2103, n);
-//
-//    // 8
+
+    // 8
 //    ull *result = exercise6_8_9(856, 9, 3, 385, n);
-//
+
     // 9
 //    ull *result = exercise6_8_9(187, 8, 5, 119, n);
-//
+
+    // განაკომენტარეთ შედეგების სანახავად
     for (int i = 0; i < n; i++) {
         printf("%llu\n", result[i]);
     }
 
     free(result);
-
-    return 0;
 }
