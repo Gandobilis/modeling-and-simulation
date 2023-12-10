3ceba7b0f31be6e3d34e3d40bb4d88946c400469
Gandobilis <lashadeveloper@gmail.com>
2023-12-10 13:37:31 +0400

    მეექვსე სავარჯიშო შევასწორე.

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 typedef unsigned long long ull;
 
@@ -25,20 +24,20 @@ double *exercise4(ull a, ull c, ull b, ull z0, ull n) {
 
     for (int i = 0; i < n; i++) {
         z = (a * z + c) << x >> x;
-        result[i] = (double) z / pow(2, (double) b);
+        result[i] = (double) z / (1u << b);
     }
 
     return result;
 }
 
-ull *exercise6(ull a, ull b, ull q, ull z0, ull n) {
-    ull *result = (ull *) malloc(sizeof(ull) * n);
+double *exercise6(ull a, ull b, ull q, ull z0, ull n) {
+    double *result = (double *) malloc(sizeof(double) * n);
 
     ull z = z0;
 
     for (int i = 0; i < n; i++) {
-        result[i] = z;
         z = exercise3(a * z, (1u << b) - q);
+        result[i] = (double) z / (double) ((1u << b) - q);
     }
 
     return result;
@@ -55,16 +54,18 @@ int main() {
     // ელემენტთა რაოდენობა გენერირებულ მიმდევრობაში
     ull n = 1000;
 
-    double *result = exercise4(1031, 113, 10, 1103, n);
+    // 4
+//    double *result = exercise4(1031, 113, 10, 1103, n);
 
     // 6
-//    ull *result = exercise6_8_9(1604, 11, 3, 2103, n);
+//    double *result = exercise6(1604, 11, 3, 2103, n);
 
-    for (int i = 0; i < n; i++) {
-        printf("%f\n", result[i]);
-    }
-
-    free(result);
+//     განაკომენტარეთ შედეგის სანახავად
+//    for (int i = 0; i < n; i++) {
+//        printf("%f\n", result[i]);
+//    }
+//
+//    free(result);
 
     return 0;
 }
